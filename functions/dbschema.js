977185083d901f2e35fba7514235d11ab0abac31
jaybee4real <okorojijaybee1@gmail.com>
2020-10-03 22:44:18 +0100

    _changed timestamp to string, created dbschema

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the body',
+            createdAt: "2020-10-03T21:38:41.109Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
