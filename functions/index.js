9a296727a4aabb59f36ba7181011891711a720c7
jaybee4real <okorojijaybee1@gmail.com>
2020-10-04 14:04:46 +0100

    _authorization function

--- functions/index.js
+++ functions/index.js
@@ -37,11 +37,45 @@ app.get("/screams", (req, res) => {
       });
       return res.json(screams);
     })
-
     .catch((err) => console.error(err));
 });
 
-app.post("/screams", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No Token Found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.userHandle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error While Verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+
+/////////////Post Screams or Posts//////////
+app.post("/screams", FBAuth, (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.user.userHandle,
@@ -154,10 +188,11 @@ app.post("/login", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if(err.code === "auth/wrong-password"){
-        return res.status(403).json({ general : "Wrong credentials, Please Try Again"})
-      }
-       else return res.status(500).json({ error: err.code });
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({ general: "Wrong credentials, Please Try Again" });
+      } else return res.status(500).json({ error: err.code });
     });
 });
 
