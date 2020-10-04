53d974760e7c64cd8d04d1a329924551338d251f
jaybee4real <okorojijaybee1@gmail.com>
2020-10-04 15:30:54 +0100

    _major code refractoring and seperation

---
+++ functions/utilities/config.js
@@ -0,0 +1,10 @@
+module.exports = (firebaseConfig = {
+    apiKey: "AIzaSyAdC0RBmqCgT99jmX7XtSFQyB9MDy_TP64",
+    authDomain: "screamers-5a283.firebaseapp.com",
+    databaseURL: "https://screamers-5a283.firebaseio.com",
+    projectId: "screamers-5a283",
+    storageBucket: "screamers-5a283.appspot.com",
+    messagingSenderId: "916491392774",
+    appId: "1:916491392774:web:9ac2554cd4c38910f2f087",
+    measurementId: "G-JX1X4354DT",
+  });
---
+++ functions/utilities/fbAuth.js
@@ -0,0 +1,38 @@
+const admin = require('./admin')
+
+
+module.exports = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No Token Found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.userHandle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error While Verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+
+/////////////Post Screams or Posts//////////
